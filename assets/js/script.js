2d80a666820e7dfa6db472c34ae84a4375a54000
Malcolm MikaZuki <malcolm.wiredu@gmail.com>
2021-12-23 20:14:04 +0000

    javascript addition

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,39 @@
-console.log("Connected!")
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+    
+    for (let i=0; i < buttons.length; i++){
+        
+    }
+})
+
+function runGame(){
+
+}
+
+function checkAnswer(){
+
+}
+
+function calculateCorrectAnswer(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionQuestion(){
+
+}
+
+function displaySubtractQuestion(){
+
+}
+
+function displayMultiplyQuestion(){
+
+}
